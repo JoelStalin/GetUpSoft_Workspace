5a49809c583d72575ad6f9e5e8a860f89583b21d
Joel Stalin Martinez Espinal <joelstalin2105@gmail.com>
2026-05-24 09:25:28 -0400

    chore(orca-client): ignore packaged desktop binaries from git

--- apps/orca-client-gateway/apps/client-desktop/dist-packager/GetUpSoft-Orca-Agent-win32-x64/resources/app/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("orcaDesktop", {
-  getState: () => ipcRenderer.invoke("state:get"),
-  setState: (state) => ipcRenderer.invoke("state:set", state),
-  openExternal: (url) => ipcRenderer.invoke("shell:openExternal", url)
-});
